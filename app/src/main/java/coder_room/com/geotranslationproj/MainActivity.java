ee4415012f3e2b9137253758c53a362536b121c1
lianghangbin <lianghangbin@weixinhai.com>
2015-08-31 15:14:06 +0800

    初始化

---
+++ app/src/main/java/coder_room/com/geotranslationproj/MainActivity.java
@@ -0,0 +1,14 @@
+package coder_room.com.geotranslationproj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
